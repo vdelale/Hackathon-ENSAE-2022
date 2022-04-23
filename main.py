26cd58fb42b9b5bb6f1715de6f98c296630f11e6
Thomas Kientz <thomas@kientz.net>
2022-04-23 10:12:46 +0200

    mmmm

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from computer_vision.main import poster_analysis
 from gender.gender_feature import *
 import pandas as pd
+import pickle
 
 df = pd.read_json("data/tmdb_data.json")
 df = df.head()
-
+clf = pickle.load("xgb_reg.pkl")
 
 def preprocessing(df):
     merged_df = poster_analysis(df)
-    print(merged_df.shape)
     merged_df = add_infos(merged_df)
-    print(merged_df.shape)
     merged_df = genderAnalysis(merged_df)
-    print(merged_df.shape)
     return merged_df
 
 
 # require : imdb_id
-def predict(df):
+def predict_bechdel(df):
     df = preprocessing(df)
+    
 
 if __name__ == "__main__":
-    predict(df)
+    predict_bechdel(df)
