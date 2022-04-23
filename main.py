64d66a2f3e01ce4b609aa75dd9a61f8d6637f9d2
Vincent Delale <vincent10.delale@orange.fr>
2022-04-23 10:05:05 +0200

    Updated main script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from computer_vision.main import poster_analysis
+from gender.gender_feature import *
 import pandas as pd
 
 df = pd.read_json("data/tmdb_data.json")
@@ -7,13 +8,17 @@ df = df.head()
 
 def preprocessing(df):
     merged_df = poster_analysis(df)
-    return df
+    print(merged_df.shape)
+    merged_df = add_infos(merged_df)
+    print(merged_df.shape)
+    merged_df = genderAnalysis(merged_df)
+    print(merged_df.shape)
+    return merged_df
 
 
 # require : imdb_id
 def predict(df):
     df = preprocessing(df)
-
 
 if __name__ == "__main__":
     predict(df)
